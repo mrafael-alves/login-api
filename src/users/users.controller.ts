65fca939f1f7a89dfd07335d180227edd2567a40
Rafael Alves <rafa_al_m@hotmail.com>
2023-12-12 17:51:32 -0300

    Serviço e Controller de usuários

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dto/register.dto';
+import { User } from './models/users.model';
+import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+interface ILogin {
+    name: string;
+    jwtToken: string;
+    email: string
+}
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor (private readonly usersService: UsersService) {}
+
+    @Post('register')
+    @HttpCode(HttpStatus.CREATED)
+    public async register(@Body() registerDto: RegisterDto): Promise<User> {
+        return this.usersService.register(registerDto);
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    public async login(@Body() loginDto: LoginDto): Promise<ILogin> {
+        return this.usersService.login(loginDto);
+    }
+
+    @Get()
+    @UseGuards(AuthGuard('jwt'))
+    @HttpCode(HttpStatus.OK)
+    public async findAll(): Promise<User[]> {
+        return this.usersService.findAll();
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { Model } from 'mongoose';
+import { User } from './models/users.model';
+import { AuthService } from 'src/auth/auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+interface ILogin {
+    name: string;
+    jwtToken: string;
+    email: string
+}
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel('User')
+        private readonly usersModel: Model<User>,
+        private readonly authService: AuthService,
+    ) {}
+
+    public async register(registerDto: RegisterDto): Promise<User> {
+        const user = new this.usersModel(registerDto);
+        return user.save();
+    }
+
+    public async login(loginDto: LoginDto): Promise<ILogin> {
+        const user = await this.findByEmail(loginDto.email);
+        const match = await this.checkPassword(loginDto.password, user);
+
+        if (!match) {
+            throw new NotFoundException('E-mail or password is invalid.');
+        }
+
+        const jwtToken = await this.authService.createAccessToken(user._id);
+
+        return {name: user.name, jwtToken, email: user.email};
+    }
+
+    private async findByEmail(email: string): Promise<User> {
+        const user = await this.usersModel.findOne({email});
+        if (!user) {
+            throw new NotFoundException('Email not found.');
+        }
+
+        return user;
+    }
+
+    public async findAll(): Promise<User[]> {
+        return this.usersModel.find();
+    }
+
+    private async checkPassword(password: string, user: User): Promise<boolean> {        
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            throw new NotFoundException('Password not found.');
+        }
+
+        return match;
+    }
+}
